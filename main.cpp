868f1f289693f7a0ecfc80bf300b17c4921c9f8d
CipollinoVP <yurich2002Vadim@gmail.com>
2022-10-29 08:58:21 +0300

    comit1_omp_srt_parallel_copy

--- main.cpp
+++ main.cpp
@@ -76,11 +76,11 @@ void LU_parallel(double* &A, int n, int m){
         return;
     }
     for (int i = 0; i < std::min(n-1,m); ++i) {
-#pragma omp parallel for default(none) shared(A,i,n,m)
         for (int j = i+1; j < n; ++j) {
             A[j*m+i] = A[j*m+i]/A[i*m+i];
         }
         if (i<m){
+#pragma omp parallel for default(none) shared(A,i,n,m)
             for (int j = i+1; j < n; ++j) {
                 for (int k = i+1; k < m; ++k) {
                     A[j*m+k]=A[j*m+k]-A[j*m+i]*A[i*m+k];
