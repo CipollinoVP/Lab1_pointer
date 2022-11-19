2f30389d31f14c8c9c83c7359efe07a9991b97db
CipollinoVP <yurich2002Vadim@gmail.com>
2022-11-19 13:12:04 +0300

    comit1_omp_srt_parallel_copy

--- main.cpp
+++ main.cpp
@@ -66,12 +66,8 @@ void LU(double* A, const int n, int m) {
     for (int i = 0; i < std::min(n-1,m); ++i) {
         for (int j = i+1; j < n; ++j) {
             A[j*m+i] = A[j*m+i]/A[i*m+i];
-        }
-        if (i<m){
-            for (int j = i+1; j < n; ++j) {
-                for (int k = i+1; k < m; ++k) {
-                    A[j*m+k]=A[j*m+k]-A[j*m+i]*A[i*m+k];
-                }
+            for (int k = i+1; k < m; ++k) {
+                A[j*m+k]=A[j*m+k]-A[j*m+i]*A[i*m+k];
             }
         }
     }
@@ -87,13 +83,8 @@ void LU_parallel(double* A, const int n, const int m)
 #pragma omp parallel for default(none) shared(A,i,n,m)
         for (int j = i+1; j < n; ++j) {
             A[j*m+i] = A[j*m+i]/A[i*m+i];
-        }
-        if (i<m){
-#pragma omp parallel for default(none) shared(A,i,n,m)
-            for (int j = i+1; j < n; ++j) {
-                for (int k = i+1; k < m; ++k) {
-                    A[j*m+k]=A[j*m+k]-A[j*m+i]*A[i*m+k];
-                }
+            for (int k = i+1; k < m; ++k) {
+                A[j*m+k]=A[j*m+k]-A[j*m+i]*A[i*m+k];
             }
         }
     }
